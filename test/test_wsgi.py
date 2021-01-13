0f3a935e75e41502e64b6c4f0fa9cae3b5e221af
maple-q <774151370@qq.com>
2021-01-13 22:15:06 +0800

    [ADD]完成路由注册功能，用户可使用框架提供的装饰器以及父类来绑定url和视图函数

--- test/test_wsgi.py
+++ test/test_wsgi.py
@@ -2,14 +2,52 @@
 # coding: utf-8
 from wsgiref.simple_server import WSGIServer, WSGIRequestHandler
 
-from tramp.tramp import Tramp
+from tramp.tramp import Tramp, BaseController, register, r
+
+
+tramp = Tramp()
+
+
+class UserController(BaseController):
+
+    __prefix__ = '/user'
+
+    @register('/login', method=['GET', 'POST'])
+    def login(self, request):
+        pass
+
+    @register('/logout', method=['GET'])
+    def logout(self, request):
+        pass
+
+
+class BookController(BaseController):
+
+    __prefix__ = '/book'
+
+    @register('/add', method=['POST'])
+    def add_book(self, request):
+        return 'add book'
+
+    @register('/delete', method=['POST'])
+    def delete_book(self, request):
+        return 'delete book'
+
+    @register('/update', method=['POST'])
+    def update_book(self, request):
+        return 'update book'
+
+    @register('/query', method=['GET'])
+    def query_book(self, request):
+        return 'query books'
 
 
 if __name__ == '__main__':
     host = '127.0.0.1'
     port = 9000
     server = WSGIServer((host, port), WSGIRequestHandler)
-    server.set_app(Tramp)
+    server.set_app(tramp)
     print('Server Listen at: {}:{}'.format(host, port))
+    print(r)
 
     server.serve_forever()
--- tramp/tramp.py
+++ tramp/tramp.py
@@ -1,19 +1,93 @@
 #! /usr/bin/python
 # coding: utf-8
+import functools
+
+from collections import OrderedDict
+
+
+DEFAULT_METHOD = 'GET'
 
 
 class Tramp(object):
     """
     Tramp is a python web framework that implements wsgi protocol.
-    Tramp Class is the interface between the web server and web framework, So when deploy your application,
-    you can provide the Tramp Class to the server.
+    Tramp Instance is the interface between the web server and web framework, So when deploy your application,
+    you can provide the Tramp Instance to the server.
     """
-    def __init__(self, environ, start_response):
-        self.environ = environ
-        self.start_response = start_response
+    def __init__(self):
+        # url map
+        self.url_map = dict()
 
-    def __iter__(self):
+        # prefix map
+        self.prefix_map = OrderedDict()
+
+    def __call__(self, environ, start_response):
         status = '200 OK'
         response_headers = [('Content-type', 'text/plain')]
-        self.start_response(status, response_headers)
-        yield b'Hello World, Tramp!'
+        start_response(status, response_headers)
+
+        response = self.url_map[environ['PATH_INFO']]('')
+        yield response.encode()
+
+
+class Router(object):
+    def __init__(self):
+        """
+        {
+            "/user/login": {
+                "method": ["GET", "POST],
+                "view": view_function
+            }
+        }
+        """
+        self.route_map = OrderedDict()
+
+    def add_url_rule(self, url, view_function, method):
+        if url in self.route_map:
+            raise Exception('Existing url: {}'.format(url))
+
+        self.route_map[url] = view_function
+
+    def __str__(self):
+        router_size = len(self.route_map)
+        routes = []
+
+        for url in self.route_map:
+            routes.append(url)
+
+        return '<RouterSize: {}, Routers: {}>'.format(router_size, '[' + ', '.join(routes) + ']')
+
+
+r = Router()
+
+
+class MetaController(type):
+    def __new__(mcs, name, bases, attrs):
+        if name == 'BaseController':
+            return type.__new__(MetaController, name, bases, attrs)
+
+        prefix = attrs.pop('__prefix__', None)
+        if not prefix:
+            prefix = '/'
+
+        for name, value in attrs.items():
+            if hasattr(value, 'url') and hasattr(value, 'method'):
+                r.add_url_rule(prefix + getattr(value, 'url'), value, getattr(value, 'method'))
+
+        return type.__new__(MetaController, name, bases, attrs)
+
+
+class BaseController(metaclass=MetaController):
+    pass
+
+
+def register(url, method):
+    def func(view):
+        def inner(*args, **kwargs):
+            view(*args, **kwargs)
+
+        setattr(inner, 'url', url)
+        setattr(inner, 'method', method)
+        return inner
+
+    return func
